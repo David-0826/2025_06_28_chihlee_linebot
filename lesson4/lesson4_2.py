ad9d4c4727d70529d07c4d4e1af041962e5372b4
david660826 <david660826@gmail.com>
2025-07-19 16:03:11 +0800

    modify

---
+++ lesson4/lesson4_2.py
@@ -0,0 +1,9 @@
+# list
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+fruits.append("grape")
+fruits.append("watermelon")
+fruits.append("pineapple")
+fruits.append("mango")
+print(type(fruits))  # 顯示類型
+print("水果清單:", fruits)
